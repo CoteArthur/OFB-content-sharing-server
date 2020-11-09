d6b7dcbe24432d50dbbbc2f36c8a0461225a9fcb
CoteArthur <57949230+CoteArthur@users.noreply.github.com>
2020-11-09 20:27:16 +0100

    implemented https

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import * as cors from 'cors';
 import HomeRoute from './routes/home';
 import * as path from 'path';
 
+import * as fs from 'fs';
+import * as https from 'https';
+
 const app = express();
 
 app.use(cors());
@@ -10,9 +13,11 @@ app.use(express.json({ limit: '10MB' }));
 app.use('/files', express.static('files'));
 app.use(express.static(path.join(__dirname, '../../build')));
 
-
-const port = process.env.PORT || 25565;
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
-
+const port = 443;
+const credentials = {
+    key: fs.readFileSync(path.join(__dirname, '../sslcert/selfsigned.key')),
+    cert: fs.readFileSync(path.join(__dirname, '../sslcert/selfsigned.crt'))
+};
+https.createServer(credentials, app).listen(port, () => console.log(`Server listening on port: ${port}`));
 
 new HomeRoute(app);
